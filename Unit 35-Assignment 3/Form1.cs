3b82a6ebffc8432a7a11084fd55363b166040fc6
RC0029066 <RC0029066@CRO-27-10>
2018-06-20 11:01:09 +0100

    Added catch

--- Unit 35-Assignment 3/Form1.cs
+++ Unit 35-Assignment 3/Form1.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Unit_35_Assignment_3
 {
     public partial class Form1 : Form
     {
+        class row
+        {
+          public  Double time;
+          public Double altimeter;
+        }
+
+        List<row> table = new List<row>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv files|*csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] r = sr.ReadLine().Split(',');
+                            table.Last().time = double.Parse(r[0]);
+                            table.Last().altimeter = double.Parse(r[1]);
+                        }
+
+
+
+                    }
+
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "Failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "Incorrect format.");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "Incorrect Format.");
+                }
+            }
         }
     }
 }
