183780a795044119766f2a11a64fec951f3ee3b0
RC0029066 <RC0029066@CRO-27-10>
2018-06-29 13:05:49 +0100

    fINISHED

--- Unit 35-Assignment 3/Form1.cs
+++ Unit 35-Assignment 3/Form1.cs
@@ -17,7 +17,6 @@ namespace Unit_35_Assignment_3
         class row
         {
             public double time;
-            public double altimeter;
             public double Velocity;
             public double accleration;
             public double altitude;
@@ -33,7 +32,7 @@ namespace Unit_35_Assignment_3
 
         private void calculateVelocity()
         {
-            for (int i = 1; 1 < table.Count; i++)
+            for (int i = 1; i < table.Count; i++)
             {
                 double dh = table[i].altitude - table[i - 1].altitude;
                 double dt = table[i].time - table[i - 1].time;
@@ -44,11 +43,11 @@ namespace Unit_35_Assignment_3
 
         private void calculateAcceleration()
         {
-            for (int i = 1; 1 < table.Count; i++)
+            for (int i = 1; i < table.Count; i++)
             {
                 double dv = table[i].Velocity - table[i - 1].Velocity;
                 double dt = table[i].time - table[i - 1].time;
-                table[i].Velocity = dv / dt;
+                table[i].accleration = dv / dt;
             }
         }
 
@@ -72,7 +71,7 @@ namespace Unit_35_Assignment_3
                             table.Add(new row());
                             string[] r = sr.ReadLine().Split(',');
                             table.Last().time = double.Parse(r[0]);
-                            table.Last().altimeter = double.Parse(r[1]);
+                            table.Last().altitude = double.Parse(r[1]);
                         }
 
 
@@ -167,7 +166,7 @@ namespace Unit_35_Assignment_3
                 chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
                 Series series = new Series
                 {
-                    Name = "altitude",
+                    Name = "velocity",
                     Color = Color.Blue,
                     IsVisibleInLegend = false,
                     IsXValueIndexed = true,
@@ -177,10 +176,10 @@ namespace Unit_35_Assignment_3
                 chart1.Series.Add(series);
                 foreach (row r in table.Skip(1))
                 {
-                    series.Points.AddXY(r.time, r.altitude);
+                    series.Points.AddXY(r.time, r.Velocity);
                 }
                 chart1.ChartAreas[0].AxisX.Title = "time /s";
-                chart1.ChartAreas[0].AxisY.Title = "Altitude /m";
+                chart1.ChartAreas[0].AxisY.Title = "Velocity /m/s";
                 chart1.ChartAreas[0].RecalculateAxesScale();
 
 
@@ -198,7 +197,7 @@ namespace Unit_35_Assignment_3
                 chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
                 Series series = new Series
                 {
-                    Name = "altitude",
+                    Name = "acceleration",
                     Color = Color.Blue,
                     IsVisibleInLegend = false,
                     IsXValueIndexed = true,
@@ -208,10 +207,10 @@ namespace Unit_35_Assignment_3
                 chart1.Series.Add(series);
                 foreach (row r in table.Skip(1))
                 {
-                    series.Points.AddXY(r.time, r.altitude);
+                    series.Points.AddXY(r.time, r.accleration);
                 }
                 chart1.ChartAreas[0].AxisX.Title = "time /s";
-                chart1.ChartAreas[0].AxisY.Title = "Altitude /m";
+                chart1.ChartAreas[0].AxisY.Title = "Acceleration /m/s";
                 chart1.ChartAreas[0].RecalculateAxesScale();
 
 
